68c642153d2c264e71ccf8632c0db84f868db223
RobertM <robertjazzsax@gmail.com>
2023-10-18 00:06:45 +0100

    Update Strings

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,12 @@
+const producto = 'Monitor 20 Pulgadas ';
+// Con este método lo llevamos todo a Mayúsculas
+console.log(producto.toUpperCase());
+
+// Con este método lo convertimos todo a minúsculas
+console.log(producto.toLowerCase());
+
+const precio = 300;
+console.log(precio);
+
+// Para convertir este número a String
+console.log(precio.toString());
